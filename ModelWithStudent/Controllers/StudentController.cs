07ac6ce1948c0beb70222ba31ff0a0706a387c4f
ajinkya hake <hakeajinkya2@gmail.com>
2022-04-19 16:56:43 +0530

    Add project files.

---
+++ ModelWithStudent/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelWithStudent.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ ModelWithStudent/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelWithStudent.Models
+{
+    public class Student
+    
+        {
+            [Key]
+            [ScaffoldColumn(false)]
+            public int Id { get; set; }
+
+            [DisplayName("Employee Name")]
+            [MinLength(2)]
+            [MaxLength(20)]
+            [DataType(DataType.Text)]
+            [Required(ErrorMessage = "Name is required")]
+            public string Name { get; set; }
+
+            [DataType(DataType.EmailAddress)]
+            [DisplayName("Email Id")]
+            [Required(ErrorMessage = "Emailid is required")]
+            public string EmailId { get; set; }
+
+        }
+    }
